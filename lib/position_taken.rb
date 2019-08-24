d624136943463e1ddf3c6a21ce1c09d7dff62d98
beneficial-trainer-3965 <natbrij@gmail.com>
2019-08-24 19:34:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,20 +1,4 @@
 # code your #position_taken? method here!
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
-# code your input_to_index and move method here!
-def input_to_index(input)
-  input.to_i - 1
-end
-
-# move(board, 0, "X")
-# expected output to be ["X", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def move (board, position, char = "X")
-  board[position] = char
+def position_taken(input)
 end
