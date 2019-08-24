9e92faae7193a0a7e5b0900e58f10b816c2ea7b6
beneficial-trainer-3965 <natbrij@gmail.com>
2019-08-24 19:52:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,10 @@ def position_taken?(board, index)
   if board[index] == "" || board[index] == " "
       return false
     end
-
+  if board[index] == "nil"
+    return false
+  end
+  
   if board[index] == "X" || board[index] == "O"
     return true
   end
